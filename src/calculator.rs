4894e3854df14806a206c7ba7af8f47253c23597
H.N <hayato.n.fl@gmail.com>
2024-06-19 13:20:07 +0900

    Add test.

--- src/calculator.rs
+++ src/calculator.rs
@@ -17,3 +17,21 @@ impl Calculator {
         self.mulend1 * self.mulend2
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use rust_decimal::prelude::FromPrimitive;
+    use super::*;
+
+    #[test]
+    fn multiply_two_integer() {
+        let calculator = Calculator::new(Decimal::new(5, 0), Decimal::new(10, 0));
+        assert_eq!(calculator.calc(), Decimal::new(50, 0));
+    }
+
+    #[test]
+    fn multiply_two_float() {
+        let calculator = Calculator::new(Decimal::from_f64(2.5).unwrap(), Decimal::from_f64(2.5).unwrap());
+        assert_eq!(calculator.calc(), Decimal::from_f64(6.25).unwrap());
+    }
+}
